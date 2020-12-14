3245e107a24bc8175b64ff2f31ff0b45cb7259bc
Marc Anthony Surio <suriomanthony@gmail.com>
2020-12-14 06:01:26 -0800

    created a notes.js file and created a var called notes

---
+++ db/notes.js
@@ -0,0 +1,13 @@
+var notes = [
+    {
+        "title": "Note 1",
+        "text": "Note 1",
+        "id": 1
+    },
+    
+    {
+        "title": "Note 2",
+        "text": "Note 2",
+        "id": 2 
+    }
+]
